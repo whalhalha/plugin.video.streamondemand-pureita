8720fadf7f227ecbf4e47b5acb799b219ca26daf
Fenice82 <fenice82@gmail.com>
2015-09-06 23:49:24 +0200

    fixed eurostreaming

--- channels/eurostreaming.py
+++ channels/eurostreaming.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
-#------------------------------------------------------------
+# ------------------------------------------------------------
 # pelisalacarta - XBMC Plugin
 # Canale per eurostreaming.tv
 # http://blog.tvalacarta.info/plugin-xbmc/pelisalacarta/
-#------------------------------------------------------------
+# ------------------------------------------------------------
 import urlparse
 import re
 import sys
@@ -22,17 +22,34 @@ __language__ = "IT"
 
 DEBUG = config.get_setting("debug")
 
+host = "http://eurostreaming.tv"
+
+
 def isGeneric():
     return True
 
 
 def mainlist(item):
     logger.info("pelisalacarta.eurostreaming mainlist")
-    itemlist = []
-    itemlist.append( Item(channel=__channel__, title="[COLOR azure]Film - Archivio[/COLOR]", action="peliculas", url="http://eurostreaming.tv/category/film-in-streaming-vk-putlocker/", thumbnail="http://repository-butchabay.googlecode.com/svn/branches/eden/skin.cirrus.extended.v2/extras/moviegenres/All%20Movies.png"))
-    itemlist.append( Item(channel=__channel__, title="[COLOR azure]Serie TV[/COLOR]" , action="serietv", url="http://eurostreaming.tv/category/serie-tv-archive/", thumbnail="http://xbmc-repo-ackbarr.googlecode.com/svn/trunk/dev/skin.cirrus%20extended%20v2/extras/moviegenres/New%20TV%20Shows.png"))
-    itemlist.append( Item(channel=__channel__, title="[COLOR azure]Anime / Cartoni[/COLOR]", action="serietv", url="http://eurostreaming.tv/category/anime-cartoni-animati/", thumbnail="http://orig09.deviantart.net/df5a/f/2014/169/2/a/fist_of_the_north_star_folder_icon_by_minacsky_saya-d7mq8c8.png"))
-    itemlist.append( Item(channel=__channel__, title="[COLOR yellow]Cerca...[/COLOR]", action="search", thumbnail="http://dc467.4shared.com/img/fEbJqOum/s7/13feaf0c8c0/Search"))
+    itemlist = [Item(channel=__channel__,
+                     title="[COLOR azure]Film - Archivio[/COLOR]",
+                     action="peliculas",
+                     url="%s/category/film-in-streaming-vk-putlocker/" % host,
+                     thumbnail="http://repository-butchabay.googlecode.com/svn/branches/eden/skin.cirrus.extended.v2/extras/moviegenres/All%20Movies.png"),
+                Item(channel=__channel__,
+                     title="[COLOR azure]Serie TV[/COLOR]",
+                     action="serietv",
+                     url="%s/category/serie-tv-archive/" % host,
+                     thumbnail="http://xbmc-repo-ackbarr.googlecode.com/svn/trunk/dev/skin.cirrus%20extended%20v2/extras/moviegenres/New%20TV%20Shows.png"),
+                Item(channel=__channel__,
+                     title="[COLOR azure]Anime / Cartoni[/COLOR]",
+                     action="serietv",
+                     url="%s/category/anime-cartoni-animati/" % host,
+                     thumbnail="http://orig09.deviantart.net/df5a/f/2014/169/2/a/fist_of_the_north_star_folder_icon_by_minacsky_saya-d7mq8c8.png"),
+                Item(channel=__channel__,
+                     title="[COLOR yellow]Cerca...[/COLOR]",
+                     action="search",
+                     thumbnail="http://dc467.4shared.com/img/fEbJqOum/s7/13feaf0c8c0/Search")]
 
     return itemlist
 
@@ -45,29 +62,44 @@ def peliculas(item):
     data = scrapertools.cache_page(item.url)
 
     # Extrae las entradas (carpetas)
-    patron  = '<div class="post-thumb">\s*'
+    patron = '<div class="post-thumb">\s*'
     patron += '<a href="?([^>"]+)"?.*?title="?([^>"]+)"?.*?<img.*?src="([^>"]+)'
-    matches = re.compile(patron,re.DOTALL).findall(data)
+    matches = re.compile(patron, re.DOTALL).findall(data)
     scrapertools.printMatches(matches)
 
-    for scrapedurl,scrapedtitle,scrapedthumbnail in matches:
+    for scrapedurl, scrapedtitle, scrapedthumbnail in matches:
         scrapedplot = ""
-        scrapedtitle=scrapertools.decodeHtmlentities(scrapedtitle.replace("Streaming",""))
+        scrapedtitle = scrapertools.decodeHtmlentities(scrapedtitle.replace("Streaming", ""))
         if scrapedtitle.startswith("Link to "):
             scrapedtitle = scrapedtitle[8:]
-        if (DEBUG): logger.info("title=["+scrapedtitle+"], url=["+scrapedurl+"], thumbnail=["+scrapedthumbnail+"]")
-        itemlist.append( Item(channel=__channel__, action="findvideos", title=scrapedtitle , url=scrapedurl , thumbnail=scrapedthumbnail , plot=scrapedplot , folder=True) )
+        if (DEBUG): logger.info(
+            "title=[" + scrapedtitle + "], url=[" + scrapedurl + "], thumbnail=[" + scrapedthumbnail + "]")
+        itemlist.append(
+            Item(channel=__channel__,
+                 action="findvideos",
+                 title=scrapedtitle,
+                 url=scrapedurl,
+                 thumbnail=scrapedthumbnail,
+                 plot=scrapedplot,
+                 folder=True))
 
     # Extrae el paginador
-    patronvideos  = '<a class="next page-numbers" href="?([^>"]+)">Avanti &raquo;</a>'
-    matches = re.compile(patronvideos,re.DOTALL).findall(data)
+    patronvideos = '<a class="next page-numbers" href="?([^>"]+)">Avanti &raquo;</a>'
+    matches = re.compile(patronvideos, re.DOTALL).findall(data)
     scrapertools.printMatches(matches)
 
-    if len(matches)>0:
-        scrapedurl = urlparse.urljoin(item.url,matches[0])
-        itemlist.append( Item(channel=__channel__, action="peliculas", title="[COLOR orange]Successivo >>[/COLOR]" , url=scrapedurl, thumbnail="http://2.bp.blogspot.com/-fE9tzwmjaeQ/UcM2apxDtjI/AAAAAAAAeeg/WKSGM2TADLM/s1600/pager+old.png" , folder=True) )
+    if len(matches) > 0:
+        scrapedurl = urlparse.urljoin(item.url, matches[0])
+        itemlist.append(
+            Item(channel=__channel__,
+                 action="peliculas",
+                 title="[COLOR orange]Successivo >>[/COLOR]",
+                 url=scrapedurl,
+                 thumbnail="http://2.bp.blogspot.com/-fE9tzwmjaeQ/UcM2apxDtjI/AAAAAAAAeeg/WKSGM2TADLM/s1600/pager+old.png",
+                 folder=True))
 
     return itemlist
+
 
 def serietv(item):
     logger.info("pelisalacarta.eurostreaming peliculas")
@@ -75,71 +107,126 @@ def serietv(item):
 
     # Descarga la pagina
     data = scrapertools.cache_page(item.url)
-    
+
     # Extrae las entradas (carpetas)
-    patron  = '<div class="post-thumb">\s*'
+    patron = '<div class="post-thumb">\s*'
     patron += '<a href="?([^>"]+)"?.*?title="?([^>"]+)"?.*?<img.*?src="([^>"]+)'
-    matches = re.compile(patron,re.DOTALL).findall(data)
+    matches = re.compile(patron, re.DOTALL).findall(data)
     scrapertools.printMatches(matches)
 
-    for scrapedurl,scrapedtitle,scrapedthumbnail in matches:
+    for scrapedurl, scrapedtitle, scrapedthumbnail in matches:
         scrapedplot = ""
-        scrapedtitle=scrapertools.decodeHtmlentities(scrapedtitle.replace("Streaming",""))
+        scrapedtitle = scrapertools.decodeHtmlentities(scrapedtitle.replace("Streaming", ""))
         if scrapedtitle.startswith("Link to "):
             scrapedtitle = scrapedtitle[8:]
-        if (DEBUG): logger.info("title=["+scrapedtitle+"], url=["+scrapedurl+"], thumbnail=["+scrapedthumbnail+"]")
-        itemlist.append( Item(channel=__channel__, action="findvid_serie", title=scrapedtitle , url=scrapedurl , thumbnail=scrapedthumbnail , plot=scrapedplot , folder=True) )
+        if (DEBUG): logger.info(
+            "title=[" + scrapedtitle + "], url=[" + scrapedurl + "], thumbnail=[" + scrapedthumbnail + "]")
+        itemlist.append(
+            Item(channel=__channel__,
+                 action="episodios",
+                 title=scrapedtitle,
+                 url=scrapedurl,
+                 thumbnail=scrapedthumbnail,
+                 plot=scrapedplot,
+                 folder=True))
 
     # Extrae el paginador
-    patronvideos  = '<a class="next page-numbers" href="?([^>"]+)">Avanti &raquo;</a>'
-    matches = re.compile(patronvideos,re.DOTALL).findall(data)
+    patronvideos = '<a class="next page-numbers" href="?([^>"]+)">Avanti &raquo;</a>'
+    matches = re.compile(patronvideos, re.DOTALL).findall(data)
     scrapertools.printMatches(matches)
 
-    if len(matches)>0:
-        scrapedurl = urlparse.urljoin(item.url,matches[0])
-        itemlist.append( Item(channel=__channel__, action="serietv", title="[COLOR orange]Successivo >>[/COLOR]" , url=scrapedurl, thumbnail="http://2.bp.blogspot.com/-fE9tzwmjaeQ/UcM2apxDtjI/AAAAAAAAeeg/WKSGM2TADLM/s1600/pager+old.png" , folder=True) )
+    if len(matches) > 0:
+        scrapedurl = urlparse.urljoin(item.url, matches[0])
+        itemlist.append(
+            Item(channel=__channel__,
+                 action="serietv",
+                 title="[COLOR orange]Successivo >>[/COLOR]",
+                 url=scrapedurl,
+                 thumbnail="http://2.bp.blogspot.com/-fE9tzwmjaeQ/UcM2apxDtjI/AAAAAAAAeeg/WKSGM2TADLM/s1600/pager+old.png",
+                 folder=True))
 
     return itemlist
 
-def search(item,texto):
-    logger.info("[eurostreaming.py] "+item.url+" search "+texto)
-    item.url = "http://eurostreaming.tv/?s="+texto
+
+def search(item, texto):
+    logger.info("[eurostreaming.py] " + item.url + " search " + texto)
+    item.url = "%s/?s=%s" % (host, texto)
     try:
         return serietv(item)
     # Se captura la excepción, para no interrumpir al buscador global si un canal falla
     except:
         import sys
         for line in sys.exc_info():
-            logger.error( "%s" % line )
+            logger.error("%s" % line)
         return []
 
 
-def findvid_serie(item):
-    logger.info("[eurostreaming.py] findvideos")
+def episodios(item):
+
+    def load_episodios():
+        for data in match.split('<br/>'):
+            ## Extrae las entradas
+            end = data.find('<a ')
+            if end > 0:
+                scrapedtitle = scrapertools.find_single_match(data[:end], '\d+[^\d]+\d+')
+                itemlist.append(
+                    Item(channel=__channel__,
+                         action="findvid_serie",
+                         title="[COLOR azure]" + scrapedtitle + " (" + lang_title + ")" + "[/COLOR]",
+                         url=item.url,
+                         thumbnail=item.thumbnail,
+                         extra=data,
+                         fulltitle=item.title,
+                         show=item.title))
+
+    logger.info("[eurostreaming.py] episodios")
 
     itemlist = []
 
     ## Descarga la página
     data = scrapertools.cache_page(item.url)
+
+    patron = r"onclick=\"top.location=atob\('([^']+)'\)\""
+    b64_link = scrapertools.find_single_match(data, patron)
+    if b64_link != '':
+        import base64
+        data = scrapertools.cache_page(base64.b64decode(b64_link))
+
+    patron = r'<a href="(%s/\?p=\d+)">' % host
+    link = scrapertools.find_single_match(data, patron)
+    if link != '':
+        data = scrapertools.cache_page(link)
+
     data = scrapertools.decodeHtmlentities(data)
 
-    patron1 = '<div class="su-spoiler-content su-clearfix" style="display:none">(.+?)</div></div>'
-    matches1 = re.compile(patron1, re.DOTALL).findall(data)
-    for match1 in matches1:
-        for data in match1.split('<br/>'):
-            ## Extrae las entradas
-            scrapedtitle = data.split('<a ')[0]
-            scrapedtitle = re.sub(r'<[^>]*>', '', scrapedtitle)
-            li = servertools.find_video_items(data=data)
+    patron = '</span>([^<]+)</div><div class="su-spoiler-content su-clearfix" style="display:none">(.+?)</div></div>'
+    matches = re.compile(patron, re.DOTALL).findall(data)
+    for lang_title, match in matches:
+        lang_title = 'SUB ITA' if 'SUB' in lang_title.upper() else 'ITA'
+        load_episodios()
 
-            for videoitem in li:
-                videoitem.title = scrapedtitle + videoitem.title
-                videoitem.fulltitle = item.fulltitle
-                videoitem.thumbnail = item.thumbnail
-                videoitem.show = item.show
-                videoitem.plot = item.plot
-                videoitem.channel = __channel__
+    patron = '<li><span style="[^"]+"><a onclick="[^"]+" href="[^"]+">([^<]+)</a>(?:</span>\s*<span style="[^"]+"><strong>([^<]+)</strong>)?</span>(.*?)</div>\s*</li>'
+    matches = re.compile(patron, re.DOTALL).findall(data)
+    for lang_title1, lang_title2, match in matches:
+        lang_title = 'SUB ITA' if 'SUB' in (lang_title1+lang_title2).upper() else 'ITA'
+        load_episodios()
 
-            itemlist.extend(li)
+    return itemlist
+
+
+def findvid_serie(item):
+    logger.info("[eurostreaming.py] findvideos")
+
+    ## Descarga la página
+    data = item.extra
+
+    itemlist = servertools.find_video_items(data=data)
+    for videoitem in itemlist:
+        videoitem.title = item.title + videoitem.title
+        videoitem.fulltitle = item.fulltitle
+        videoitem.thumbnail = item.thumbnail
+        videoitem.show = item.show
+        videoitem.plot = item.plot
+        videoitem.channel = __channel__
 
     return itemlist
