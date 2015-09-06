04b44e008b2ba4bdef414e9134b60b29e8bea56a
Fenice82 <fenice82@gmail.com>
2015-09-06 23:53:50 +0200

    fixed rapidvideo.py

--- servers/rapidvideo.py
+++ servers/rapidvideo.py
@@ -10,31 +10,28 @@ import urllib,re
 from core import scrapertools
 from core import logger
 
-from lib.jsbeautifier.unpackers import packer
+from lib.jsbeautifier.unpackers  import packer
 
 def test_video_exists( page_url ):
     logger.info( "[rapidvideo.py] test_video_exists(page_url='%s')" % page_url )
-
-    page_url = page_url.replace( '.tv/', '.org/' )
 
     video_id = scrapertools.get_match( page_url, 'org/([A-Za-z0-9]+)' )
     url = 'http://www.rapidvideo.org/embed-%s-607x360.html' % video_id
 
     data = scrapertools.cache_page( url )
 
-    if "Il file è stato rimosso da RapidVideo" in data:
-        return False, "Il file non esiste o è stato rimosso da RapidVideo."
+    if "The file was removed from RapidVideo" in data:
+        return False, "The file not exists or was removed from RapidVideo."
 
     return True, ""
 
 def get_video_url( page_url, premium = False, user="", password="", video_password="" ):
     logger.info( "[rapidvideo.py] url=" + page_url )
 
-    page_url = page_url.replace( '.tv/', '.org/' )
-
     video_id = scrapertools.get_match( page_url, 'org/([A-Za-z0-9]+)' )
     url = 'http://www.rapidvideo.org/embed-%s-607x360.html' % video_id
 
+    #data = scrapertools.cache_page( url ).replace( 'TMPL_VAR|', '' )
     data = scrapertools.cache_page( url )
 
     packed = scrapertools.get_match( data, "<script type='text/javascript'>eval.function.p,a,c,k,e,.*?</script>" )
@@ -55,36 +52,25 @@ def get_video_url( page_url, premium = False, user="", password="", video_passwo
 
     return video_urls
 
-# Encuentra vÃ­deos de este servidor en el texto pasado
+# Encuentra vídeos de este servidor en el texto pasado
 def find_videos( data ):
-
     encontrados = set()
     devuelve = []
- 
-    #http://www.rapidvideo.tv/embed-xr1nb7cfh58a-607x360.html
-    #http://www.rapidvideo.org/embed-xr1nb7cfh58a-607x360.html
-    #http://www.rapidvideo.tv/xr1nb7cfh58a
+            
     #http://www.rapidvideo.org/xr1nb7cfh58a
+    patronvideos  = 'rapidvideo.org/([A-Za-z0-9]+)'
+    logger.info( "[rapidvideo.py] find_videos #" + patronvideos + "#" )
+    matches = re.compile( patronvideos, re.DOTALL ).findall( data )
 
-    patterns = [
-        'rapidvideo[^/]+/([A-Z0-9]+)',
-        'rapidvideo[^/]+/[^\-]+\-([^\-]+)\-'
-    ]
-
-    for pattern in patterns:
-
-        logger.info( "[rapidvideo.py] find_videos #" + pattern + "#" )
-        matches = re.compile( pattern, re.DOTALL ).findall( data )
-
-        for match in matches:
-            titulo = "[rapidvideo]"
-            url = "http://www.rapidvideo.org/" + match
-            if url not in encontrados:
-                logger.info( "  url=" + url )
-                devuelve.append( [ titulo, url, 'rapidvideo' ] )
-                encontrados.add( url )
-            else:
-                logger.info( " url duplicada=" + url )
+    for match in matches:
+        titulo = "[rapidvideo]"
+        url = "http://www.rapidvideo.org/" + match
+        if url not in encontrados:
+            logger.info( "  url=" + url )
+            devuelve.append( [ titulo, url, 'rapidvideo' ] )
+            encontrados.add( url )
+        else:
+            logger.info( " url duplicada=" + url )
 
     return devuelve
 
