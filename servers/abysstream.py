0002662b04d20f1d6fc0f994c602214177e3f6b2
Fenice82 <fenice82@gmail.com>
2015-09-26 09:19:28 +0200

    fix abysstream

--- servers/abysstream.py
+++ servers/abysstream.py
@@ -5,13 +5,10 @@
 # http://blog.tvalacarta.info/plugin-xbmc/pelisalacarta/
 # by be4t5
 # ------------------------------------------------------------
-
-import urlparse, urllib2, urllib, re
-import os
+import re
 
 from core import scrapertools
 from core import logger
-from core import config
 from lib import mechanize
 
 
@@ -20,18 +17,18 @@ def get_video_url(page_url, premium=False, user="", password="", video_password=
     video_urls = []
 
     br = mechanize.Browser()
-    br.addheaders = [('User-agent',
-                      'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1')]
+    br.addheaders = [('User-agent', 'Mozilla/5.0 (Windows NT 6.1; rv:38.0) Gecko/20100101 Firefox/38.0')]
     br.set_handle_robots(False)
 
-    r = br.open(page_url)
+    br.open(page_url)
+    br.open(page_url)
 
     url = "../viewvideo.php"
     req = br.click_link(url=url)
     data = br.open(req)
     data = data.read()
 
-    # URL 
+    # URL
     url = scrapertools.find_single_match(data, '<source src="([^"]+)" type="video/mp4"')
     logger.info("url=" + url)
 
@@ -58,37 +55,6 @@ def find_videos(text):
         titulo = "[Abysstream]"
         url = "http://abysstream.com/videos/" + match
         if url not in encontrados:
-            logger.info("  url=" + url)
-            devuelve.append([titulo, url, 'abysstream'])
-            encontrados.add(url)
-        else:
-            logger.info("  url duplicada=" + url)
-
-    # http://cineblog01.pw/HR/go.php?id=6475
-    temp = text.split('<strong>Streaming')
-    if len(temp) > 1:
-        tem = temp[1].split('Download')
-        patronvideos = '(?:HR)/go.php\?id\=([a-zA-Z0-9]+)'
-        matches = re.compile(patronvideos, re.DOTALL).findall(tem[0])
-    else:
-        matches = []
-
-    br = mechanize.Browser()
-    br.addheaders = [('User-agent',
-                      'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1')]
-    br.set_handle_robots(False)
-    page = scrapertools.find_single_match(text, 'rel="canonical" href="([^"]+)"')
-
-    for match in matches:
-        titulo = "[Abysstream]"
-        url = "http://cineblog01.pw/HR/go.php?id=%s" % match
-        r = br.open(page)
-        req = br.click_link(url=url)
-        data = br.open(req)
-        data = data.read()
-        vid = scrapertools.find_single_match(data, 'http://abysstream.com/videos/([^"]+)"?')
-        url = "http://abysstream.com/videos/%s" % vid
-        if url not in encontrados and vid != "":
             logger.info("  url=" + url)
             devuelve.append([titulo, url, 'abysstream'])
             encontrados.add(url)
